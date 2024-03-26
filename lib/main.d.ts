b7a2f0a85882d4756d55ee84d5d350f1aa7b27fb
Amin <amin83ghasemi@gmail.com>
2024-03-26 14:55:54 +0330

    use ES modules instead of CommonJs

--- lib/main.d.ts
+++
@@ -1,7 +0,0 @@
-import { Solver } from './numerical-solver';
-import * as Sym from './sym';
-declare const _default: {
-    Solver: typeof Solver;
-    Sym: typeof Sym;
-};
-export default _default;
